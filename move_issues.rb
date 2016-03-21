1f74087b25cc915e6e5b788857efc88e7d3169a2
Georgia A <and.georgia.m@gmail.com>
2016-03-20 17:01:07 -0700

    Do some generalization & experimentation

--- move_issues.rb
+++ move_issues.rb
@@ -1,13 +1,28 @@
-#encoding:UTF-8
-#!/usr/bin/env ruby
+# encoding:UTF-8
+# !/usr/bin/env ruby
 require 'OctoKit'
-#require 'highline/import'
-
+# require 'highline/import'
 
 client = Octokit::Client.new(netrc: true)
 client.login
 
-joblint_issues = Octokit.issues('codeforamerica/nola-2016-fellows', labels: 'job-posting')
-joblint_issues.each do |issue| 
+if ARGV.empty?
+  puts 'Usage: ruby move_issues.rb source-repo target-repo label-on-issues-to-move'
+  exit 0
+end
+
+source_repo = ARGV.shift
+target_repo = ARGV.shift
+label = ARGV.shift
+
+joblint_issues = Octokit.issues(source_repo, labels: label, state: 'open')
+puts 'Issues to move'
+joblint_issues.each do |issue|
   puts issue.title
 end
+
+puts 'Existing labels'
+puts Octokit.labels(target_repo).map(&:name)
+
+puts 'Existing milestones'
+puts Octokit.milestones(source_repo).map(&:title)
